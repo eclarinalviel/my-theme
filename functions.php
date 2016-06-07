71d538dc27509c4c2578b0be481bc1a4a600c240
Viel Eclarinal <eclarinalviel@gmail.com>
2016-06-07 23:12:02 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -18,8 +18,8 @@ function mytheme_theme_setup() {
 }
 add_action( 'init', 'mytheme_theme_setup' );
 // Theme Support function
-add_theme_support('custom-background');
-add_theme_support('custom-header');
+//add_theme_support('custom-background');
+//add_theme_support('custom-header');
 add_theme_support('post-thumbnails');
 add_theme_support('post-formats', array('aside','image','video'));
 add_theme_support('html5',array('search-form','comment-form')); //activate html5 support
