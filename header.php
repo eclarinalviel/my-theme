7d9c8e4da5ee0aa858820eb0ea6869dbf24120b3
Viel Eclarinal <eclarinalviel@gmail.com>
2016-06-08 15:39:38 +0900

    Updated Nav Menu
    Display menu if there's primary nav & display just the blog title if there's none.

--- header.php
+++ header.php
@@ -5,35 +5,54 @@
         <title> Wordpress Customized Theme </title>
         <?php wp_head(); ?>
     </head>
-
 <body>
+    <?php get_header_image(); ?>
          <!-- Navigation -->
-    <nav class="nav-container" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">
-                    <?php bloginfo( 'name' ); ?>
-                </a>
+    <?php if ( has_nav_menu( 'primary' ) ) { ?>
+        <nav class="nav-container" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">
+                        <?php bloginfo( 'name' ); ?>
+                    </a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <?php 
+                        wp_nav_menu(array(
+                            'theme_location' => 'primary',
+                            'container' => false,
+                            'menu_class' => 'nav navbar-nav navbar-right'
+                            )
+                        );
+                    ?>
+                </div>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <?php 
-                    wp_nav_menu(array(
-                        'theme_location' => 'primary',
-                        'container' => false,
-                        'menu_class' => 'nav navbar-nav navbar-right'
-                        )
-                    );
-                ?>
+        </nav>
+    <?php }else{ ?>
+        <nav class="nav-container" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">
+                        <?php bloginfo( 'name' ); ?>
+                    </a>
+                </div>
+                
             </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+        </nav>
+
+    <?php } ?>
